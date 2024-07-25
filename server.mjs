a2ee6bc3793230fd70422666a7dbbd8c7942c44a
eamonnhanson <eamonn_hanson@yahoo.co.uk>
2024-07-25 15:23:20 +0200

    Update server.mjs with integrated server script

--- server.mjs
+++ server.mjs
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 
 // Enable CORS for all routes
 app.use(cors({
-    origin: 'https://www.planteenboom.nu',
+    origin: 'https://www.planteenboom.nu', // Replace with your Shopify store domain
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Root route
 app.get('/', (req, res) => {
-    res.send('Welkom bij de formulierverzendingsserver!');
+    res.send('Welkom bij de wpmmobiliteit formulierverzendingsserver!');
 });
 
 // Endpoint voor formulierverzending
@@ -51,6 +51,7 @@ app.post('/submit-form', async (req, res) => {
                     Teller: userRecord.Teller,
                     Bedrijf: userRecord.Bedrijf,
                     // Voeg andere formuliergegevens hier toe
+                    userInputs: formData
                 };
 
                 // Verzend gegevens naar Zoho Creator via webhook
@@ -72,6 +73,31 @@ app.post('/submit-form', async (req, res) => {
     } catch (error) {
         console.error('Fout bij het verifiëren van de toegangstoken of het verzenden van gegevens:', error);
         res.status(500).json({ error: 'Interne serverfout' });
+    }
+});
+
+// Proxy endpoint for Zoho Flow
+app.post('/proxy-zoho-flow', async (req, res) => {
+    const payload = req.body;
+
+    try {
+        const response = await fetch('https://flow.zoho.eu/20071889412/flow/webhook/incoming?zapikey=1001.135d0547db270fb2604b6772f9c30ac1.e1c3971a221b2993f3d850b4b348471a&isdebug=false', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        });
+
+        const data = await response.json();
+        if (!response.ok) {
+            throw new Error('Error sending data to Zoho Creator: ' + response.statusText);
+        }
+
+        res.json(data);
+    } catch (error) {
+        console.error('Error sending data to Zoho Creator:', error);
+        res.status(500).json({ error: 'Error sending data to Zoho Creator' });
     }
 });
 
